418ab2ff0774a4077c74701ddd8f3a70eccb83f9
Son Roy Almerol <sonroya.almerol@gmail.com>
2021-07-22 13:38:24 +0800

    add rss feeds

---
+++ pages/api/rss/montage.js
@@ -0,0 +1,94 @@
+import slugGenerator from '@/utils/slugGenerator';
+
+import WPGraphQL from '@/utils/wpgraphql';
+import { gql } from 'graphql-request';
+import { Feed } from 'feed';
+
+export default async (req, res) => {
+  try {
+    const data = await WPGraphQL.request(
+      gql`
+        query Articles {
+          posts(first: 20, where: { categoryId: 31 }) {
+            pageInfo {
+              hasNextPage
+              hasPreviousPage
+              startCursor
+              endCursor
+            }
+            nodes {
+              title(format: RENDERED)
+              slug
+              date
+              coauthors {
+                nodes {
+                  firstName
+                  lastName
+                  email
+                  databaseId
+                }
+              }
+              content
+              excerpt
+              categories {
+                nodes {
+                  name
+                  databaseId
+                  slug
+                }
+              }
+              databaseId
+              featuredImage {
+                node {
+                  sourceUrl(size: LARGE)
+                }
+              }
+            }
+          }
+        }            
+      `,
+    );
+
+    const feed = new Feed({
+      title: 'Montage',
+      description: 'Welcome to the official student publication of AdDU. Here is a list of Montage written by Atenews.',
+      id: 'montage',
+      link: 'https://atenews.ph/api/rss/montage',
+      language: 'en',
+      feedLinks: {
+        rss2: 'https://atenews.ph/api/rss/montage',
+      },
+      author: {
+        name: 'Atenews',
+        email: 'atenews@addu.edu.ph',
+        link: 'https://atenews.ph/',
+      },
+    });
+
+    // Create each URL row
+    data.posts.nodes.forEach((post) => {
+      feed.addItem({
+        title: post.title,
+        id: `https://atenews.ph${slugGenerator(post)}`,
+        link: `https://atenews.ph${slugGenerator(post)}`,
+        description: post.excerpt,
+        content: post.content,
+        author: post.coauthors.nodes.map((author) => ({
+          name: `${author.firstName} ${author.lastName}`,
+          email: author.email,
+        })),
+        date: new Date(post.date),
+      });
+    });
+
+    // Change headers
+    res.writeHead(200, {
+      'Content-Type': 'application/xml',
+    });
+
+    // Display output to user
+    res.end(feed.rss2());
+  } catch (e) {
+    res.send(JSON.stringify(e));
+  }
+};
---
+++ pages/api/rss/university-news.js
@@ -0,0 +1,94 @@
+import slugGenerator from '@/utils/slugGenerator';
+
+import WPGraphQL from '@/utils/wpgraphql';
+import { gql } from 'graphql-request';
+import { Feed } from 'feed';
+
+export default async (req, res) => {
+  try {
+    const data = await WPGraphQL.request(
+      gql`
+        query Articles {
+          posts(first: 20, where: { categoryId: 20 }) {
+            pageInfo {
+              hasNextPage
+              hasPreviousPage
+              startCursor
+              endCursor
+            }
+            nodes {
+              title(format: RENDERED)
+              slug
+              date
+              coauthors {
+                nodes {
+                  firstName
+                  lastName
+                  email
+                  databaseId
+                }
+              }
+              content
+              excerpt
+              categories {
+                nodes {
+                  name
+                  databaseId
+                  slug
+                }
+              }
+              databaseId
+              featuredImage {
+                node {
+                  sourceUrl(size: LARGE)
+                }
+              }
+            }
+          }
+        }            
+      `,
+    );
+
+    const feed = new Feed({
+      title: 'University News',
+      description: 'Welcome to the official student publication of AdDU. Here is a list of University News written by Atenews.',
+      id: 'university-news',
+      link: 'https://atenews.ph/api/rss/university-news',
+      language: 'en',
+      feedLinks: {
+        rss2: 'https://atenews.ph/api/rss/university-news',
+      },
+      author: {
+        name: 'Atenews',
+        email: 'atenews@addu.edu.ph',
+        link: 'https://atenews.ph/',
+      },
+    });
+
+    // Create each URL row
+    data.posts.nodes.forEach((post) => {
+      feed.addItem({
+        title: post.title,
+        id: `https://atenews.ph${slugGenerator(post)}`,
+        link: `https://atenews.ph${slugGenerator(post)}`,
+        description: post.excerpt,
+        content: post.content,
+        author: post.coauthors.nodes.map((author) => ({
+          name: `${author.firstName} ${author.lastName}`,
+          email: author.email,
+        })),
+        date: new Date(post.date),
+      });
+    });
+
+    // Change headers
+    res.writeHead(200, {
+      'Content-Type': 'application/xml',
+    });
+
+    // Display output to user
+    res.end(feed.rss2());
+  } catch (e) {
+    res.send(JSON.stringify(e));
+  }
+};
